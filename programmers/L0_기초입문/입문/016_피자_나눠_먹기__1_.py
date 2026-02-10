40828cb58e2222145b3c953365713a62dae82821
hrbffl <gyuri8798@gmail.com>
2026-02-10 09:39:47 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,9 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 한규리
+# 작성일: 2026. 02. 10. 09:39:46
+
+def solution(n):
+    return (n - 1) // 7 + 1
